1107af72f5eb88c3a4cef8d546f27f3c0c11b61a
Jopa01 <>
2021-05-07 00:56:30 +0000

    Funcion main con acceso a menu y importacion, creacion de los mapas

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Map.h"
+#include "map.h"
 #include "funciones.h"
 #include "menu.h"
 
-int main(){
+void importarPokemonsDesdeUnArchivo(Map* mapaAlmacenamiento, Map* mapaPokedex);
 
+int main( ) {
+
+	Map * mapaAlmacenamiento = createMap(is_equal_int);
+	Map * mapaPokedex = createMap(is_equal_string);
+
+	importarPokemonsDesdeUnArchivo(mapaAlmacenamiento, mapaPokedex);
+
+	mostrarMenu(mapaAlmacenamiento, mapaPokedex);
+
+	return 0;
 
 }
